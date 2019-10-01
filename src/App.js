fb31bc2809dbd96914b52d1818d52a7f4128733a
Timur Morozov <radist@desant.biz>
2019-10-01 21:03:08 +0300

    update data & row count; wrap in component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useEffect, useState} from 'react';
+import {AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import { indexedData, columnDefs } from './data';
+import {LicenseManager} from 'ag-grid-enterprise';
+import {key} from './ag-grid-key';
+LicenseManager.setLicenseKey(key);
 
 function App() {
+  const [clicks, setClicks] = useState(0);
+
+  const trigger = useCallback(() => {
+    setClicks(clicks + 1);
+    console.log('on click', clicks);
+  }, [clicks])
+  
+  // const trigger = () => {
+  //   setClicks(clicks + 1);
+  //   console.log('on click', clicks);
+  // }
+
+  useEffect(() => {
+    console.log('init effect', clicks);
+    return () => {
+      console.log('cleanup effect', clicks)
+    }
+  }, [clicks])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {clicks}
+      <button onClick={trigger}>click</button>
+      <Grid></Grid>
     </div>
+  )
+}
+
+let viewportParams;
+let viewport = {
+  init: params => {
+    // console.log('init', params);
+    try {
+      viewportParams = params;
+      // console.log('setViewportParams', viewportParams)
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  setViewportRange: async (firstRow, lastRow) => {
+      let windowSize = lastRow - firstRow + 1;
+      if (windowSize <= 0) {
+        windowSize = 1;
+      }
+      // this.tableClient!.bounds = {firstRow, windowSize};
+  }
+};
+
+function Grid() {
+  let [rowsCount, setRowCount] = useState(0);
+
+  useEffect(() => {
+    // console.log('useEffect')
+    setInterval(() => {
+      const rc = 10000 + Math.round(Math.random()*1000);
+      setRowCount(rc);
+      if (viewportParams) {
+        viewportParams.setRowData(indexedData())
+        Promise.resolve().then(() => viewportParams.setRowCount(rc))
+      }    
+    }, 10)
+  }, [])
+
+  const onGridReady = useCallback((params) => {
+    console.log('onGridReady')
+    // startEmulation();
+  }, []);
+
+  return (
+    <>
+    <div className="ag-theme-balham" style={{ height: '500px', width: '600px'}}>
+      <AgGridReact
+          rowModelType="viewport"
+          suppressChangeDetection={true}
+          suppressColumnVirtualisation={true}
+          suppressScrollOnNewData={true}
+          viewportDatasource={viewport}
+          viewportRowModelBufferSize={20}
+          viewportRowModelPageSize={20}
+					columnDefs={columnDefs}
+          onGridReady={onGridReady}
+          >
+			</AgGridReact>
+    </div>
+    <div>{rowsCount}</div>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,104 @@
+const rowData = [
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+  {make: "Toyota", model: "Celica", price: 35000},
+  {make: "Ford", model: "Mondeo", price: 32000},
+  {make: "Porsche", model: "Boxter", price: 72000},
+];
+
+export function getData() {
+  return rowData;
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+export function indexedData() {
+  const indexedData = shuffle(rowData)
+  .reduce((acc, cur, i) => {
+    acc[i] = cur;
+    return acc;
+  }, {});
+
+  return indexedData;
+}
+
+export const columnDefs = [
+  {headerName: "Make", field: "make", pinned: 'left'},
+  {headerName: "Model", field: "model"},
+  {headerName: "Price", field: "price"}
+];
